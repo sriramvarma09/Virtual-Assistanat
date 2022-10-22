a80fbedf081cff22f84f0b706de5e92af4ad95d1
sriram varma <86653234+sriramvarma09@users.noreply.github.com>
2022-10-22 07:45:04 +0530

    Update text to speech.py

--- text to speech.py
+++ text to speech.py
@@ -4,6 +4,6 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 
 engine.setProperty('voice', voices[1].id) #change index to change voices
-engine.say('I\'m a little teapot...')
+engine.say('hii good morning')
 
 engine.runAndWait()
